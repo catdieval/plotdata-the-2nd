e5f0149b4c371e7efe5f60bd51a2eeefffbfcd4b
Catherine <catherine.dieval@hotmail.fr>
2024-11-15 11:33:54 +0100

    Used try catch to catch error in the ConvertCSVToArray component

--- Components/ConvertCSVToArray/index.js
+++ Components/ConvertCSVToArray/index.js
@@ -7,7 +7,14 @@ export default function ConvertCSVToArray(data, { header, separator } = {}) {
     separator: separator || ",",
   };
 
-  checkIfValid(data, thisOptions);
+  try {
+    checkIfValid(data, thisOptions);
+    return convertCSVToArrayOfObjects(data, thisOptions); 
 
-  return convertCSVToArrayOfObjects(data, thisOptions);
+  } catch (error) {
+    return ([]);
+  } 
+  //checkIfValid(data, thisOptions);
+
+  //return convertCSVToArrayOfObjects(data, thisOptions); 
 }
