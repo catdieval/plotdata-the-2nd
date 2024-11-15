9acc343425924bb642f2ebf4e8e51162e5d7b38f
Catherine <catherine.dieval@hotmail.fr>
2024-11-15 11:47:10 +0100

    Made various changes in the UploadData component

--- Components/UploadData/index.js
+++ Components/UploadData/index.js
@@ -5,12 +5,16 @@ import FileUploader from "../FileUploader";
 import InputTypeSubmit from "../InputTypeSubmit";
 import {ButtonContainer} from "../Navigation/styledNavigation";
 import WarningMessage from "../WarningMessage";
+import Button from "../Button";
 
 export default function UploadData({ 
   onDisableNextButton, 
   onNext, 
+  onConversion,
   fileObject, 
   onUploadFile, 
+  keynames,
+  vals,
 }) {
   // Name of file
   const fileName = fileObject?.name;
@@ -18,6 +22,20 @@ export default function UploadData({
   const handleSubmit = (event) => {
     event.preventDefault();
   };
+
+  function headerButNoData() {
+    if (keynames?.length >= 1) {
+      return vals.length < 1;
+    } else {
+      return ([]);
+    }
+  }
+
+  const headerButNoDataTemp = headerButNoData();
+
+  const hasNotCompletedStep1 = ((!fileObject) || 
+  (((keynames?.length === undefined) || (keynames?.length < 1))) ||
+  (((keynames?.length >= 1) && headerButNoDataTemp)));
 
   return (
     <>
@@ -36,11 +54,11 @@ export default function UploadData({
           4GB.
         </Paragraph>
         <Paragraph $variant="start">
-          <span> ✔ </span>The file should be comma-delimited and the decimal
+          <span> ✔ </span>The file should have a comma (,) as separator and the decimal
           separator for numbers should be a period (.).
         </Paragraph>
       </Card>
-      {((fileObject != null) && ((fileName.slice(-4)) == ".csv")) && (
+      {((fileObject != null) && ((fileName.slice(-4)) != ".csv")) && (
           <Container $centered="center">
             <WarningMessage 
               buttonMessage="OK, I will select only a csv file!"
@@ -48,6 +66,22 @@ export default function UploadData({
             />
           </Container>
         )}
+        {((fileObject != null) && ((Array.isArray(keynames)) == false)) && (
+          <Container $centered="center">
+            <WarningMessage 
+              buttonMessage="OK, I will select another file!"
+              message="The file cannot be processed! Possible reasons: it is empty OR it uses the wrong separator OR it has only 1 column. You must select a different file in order to continue!"
+            />
+          </Container>
+        )}
+        {(((keynames?.length >= 1) && headerButNoDataTemp)) && (
+          <Container $centered="center">
+            <WarningMessage 
+              buttonMessage="OK, I will select a file with both header and data!"
+              message="The file does NOT have data after the header! You must select a file with both header and data in order to continue!"
+            />
+          </Container>
+        )}          
       <form onSubmit={handleSubmit}>
         <Container $centered="center">
           <WarningMessage
@@ -56,14 +90,27 @@ export default function UploadData({
           />
           <FileUploader onUploadFile={onUploadFile} />
           {fileName ? <p>Uploaded file: {fileName}</p> : <p>No file chosen</p>}
+          <ButtonContainer>
+            <InputTypeSubmit
+              valueString="Submit my data"
+              onClick={onConversion}
+              disabled={onDisableNextButton}
+            />
+          </ButtonContainer>
+          {((keynames?.length >= 1) && (vals?.length >= 1)) &&  
+            <Paragraph>File processed successfully!</Paragraph>
+          }
         </Container>
-        <ButtonContainer>
-          <InputTypeSubmit
-            valueString="Next"
+        <br/>
+        <Container $centered="center">
+          <Button
             onClick={onNext}
-            disabled={onDisableNextButton}
-          />  
-        </ButtonContainer>
+            disabled={hasNotCompletedStep1}
+            $variant="next"
+          >
+            Next
+          </Button>
+        </Container>
       </form>
     </>
   );
